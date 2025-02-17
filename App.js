4fa5dd54cf90aede56ef554e26a2fc8dfda500e2
raviteja-nagisetti <raviteja.nagisetti20@gmail.com>
2025-02-17 12:29:49 +0530

    Revise my prev learnings

--- App.js
+++ App.js
@@ -1,22 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-	React.createElement("div", { id: "child" }, [
-		React.createElement("h1", {}, "Heading1"),
-		React.createElement("h2", {}, "Heading2"),
-	]),
-	React.createElement("div", { id: "child2" }, [
-		React.createElement("h1", {}, "Heading1"),
-		React.createElement("h2", {}, "Heading2"),
-	]),
-]);
+const heading = React.createElement("h1", {}, "Namaste React");
 
-const heading = React.createElement(
-	"h1",
-	{ id: "heading" },
-	"Hello world from React"
-);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(parent);
-root.render(parent);
+root.render(heading);
